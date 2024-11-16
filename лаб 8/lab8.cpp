362a23845ac150443c1afe7453e0137df7ef5c9d
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2024-11-16 19:57:11 +0400

    Editing 9 lab

--- лаб 8/lab8.cpp
+++ лаб 8/lab8.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main() {
-	double EPS = 10e-4, i, result = 0;
+	double EPS = 10e-4, a = 1.0/3.0, result = 0;
 	int n = 1;
-	do {
-		result += i = 1.0/((2*n-1)*(2*n+1));
+	while (a>EPS) {
+		result += a *= 1.0/((2*n-1)*(2*n+1));
 		n++;
-	} while (i>EPS);
+	}
 	cout<<result<<endl;
 }
--- лаб 9/lab9.cpp
+++ лаб 9/lab9.cpp
@@ -6,6 +6,9 @@ using namespace std;
 double f(double x) {
 	return pow(1-x, 2) / pow(x, 2);
 }
+double g(double x) {
+	return -2 * (1 - x) * x * (x + 1) / pow(x, 3);
+}
 int main() {
 	double b, a;
 	cin>>a>>b;
@@ -16,5 +19,14 @@ int main() {
 		for (int i = 0; i < n; i++) integral_new += f(a+(i+0.5)*h)*h;
 	} while (abs(integral_new - integral_old) < EPS);
 	cout << integral_new << endl;
+	
+	h = (b - a) / (n = 1000);
+    double integral = 0.0, x1, x2;
+
+    for (int i = 0; i < n; ++i) {
+        x1 = a + i * h, x2 = a + (i + 1) * h, integral += 0.5 * (g(x1) + g(x2)) * h;
+    }
+
+    cout << integral << endl;
 }
 
