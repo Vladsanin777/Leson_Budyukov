af83f9886cec29887381a7d0a1f367216697e85c
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2024-12-12 18:39:14 +0400

    10 works

---
+++ лаб 11/lab11.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	vector<int> a;
+	
+	for (int i = 1; i != 0;) {
+		cin >> i;
+		a.push_back(i);
+	}
+	reverse(a.begin(), a.end());
+	for (vector<int>::iterator i = a.begin(); i - a.begin() != a.size(); i++) {
+		cout << *i << endl;
+	}
+	return 0;
+}
--- лаб 9/lab9.cpp
+++ лаб 9/lab9.cpp
@@ -1,32 +1,39 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>
+#include <functional>
 
 using namespace std;
 
-double f(double x) {
-	return pow(1-x, 2) / pow(x, 2);
+double f_integral(double x) {
+	return pow(1 - x, 2) / pow(x, 2);
 }
-double g(double x) {
-	return -2 * (1 - x) * x * (x + 1) / pow(x, 3);
-}
-int main() {
-	double b, a;
-	cin>>a>>b;
-	double h, EPS = 0.01, integral_old, integral_new = 0;
+
+void integral(function<double(double)> f, double a, double b, double EPS) {
+	double h, integral_old, integral_new = 0;
 	int n = 1;
 	do {
-		n *= 2, h = (b-a)/n, integral_old = integral_new, integral_new = 0;
-		for (int i = 0; i < n; i++) integral_new += f(a+(i+0.5)*h)*h;
+		n *= 2, h = (b - a) / n, integral_old = integral_new, integral_new = 0;
+		for (int i = 0; i < n; i++) integral_new += f(a + (i + 0.5) * h) * h;
 	} while (abs(integral_new - integral_old) < EPS);
 	cout << integral_new << endl;
-	
-	h = (b - a) / (n = 1000);
-    double integral = 0.0, x1, x2;
-
-    for (int i = 0; i < n; ++i) {
-        x1 = a + i * h, x2 = a + (i + 1) * h, integral += 0.5 * (g(x1) + g(x2)) * h;
-    }
-
-    cout << integral << endl;
+	return;
 }
-
+void lenght_line(double a, double b, double EPS) {
+	int n = 2;
+	double L = 0, L1 = 0, h;
+	while (abs(L - L1) < EPS) {
+		L1 = L, L = 0, h = (b - a) / n;
+		for (int i = 1; i <= n; i++) {
+			L += sqrt(pow(a + h * (i - 1) - (a + i * n), 2) + pow(f_integral(a + h * (i - 1)) - f_integral(a + h * (i - 1)) - f_integral(a + i * h), 2));
+		}
+		n = pow(n, 2);
+	}
+	cout << L;
+}
+int main() {
+	double b, a, EPS;
+	cin >> a >> b >> EPS;
+	integral(f_integral, a, b, EPS);
+	lenght_line(a, b, EPS);
+	return 0;
+}
